7b3c99af9105ad11fd6c24ca5b87d15f4b63b7ce
Yunior Diaz <yunior0716@gmail.com>
2024-01-12 17:34:45 -0400

    first commit

---
+++ src/domain/use-cases/checks/check-service.ts
@@ -0,0 +1,28 @@
+interface CheckServiceInterface {
+  execute(url: string): Promise<boolean>;
+}
+
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
+export class CheckService {
+  constructor(
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
+
+  public async execute(url: string): Promise<boolean> {
+    try {
+      const req = await fetch(url);
+      if (!req.ok) {
+        throw new Error(`Error on check service ${url}`);
+      }
+      this.successCallback();
+
+      return true;
+    } catch (error) {
+      this.errorCallback(`${error}`);
+      return false;
+    }
+  }
+}
